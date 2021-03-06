f029f7ae9f359450fee580ccdd1d1c3f87b6e18b
ApxuTechTop <78413120+ApxuTechTop@users.noreply.github.com>
2021-03-06 19:55:12 +0600

    Update main.c

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -1,11 +1,26 @@
-#include "chessboard.h"
+#include "chess_read.h"
+#include <chess.h>
+#include <chessboard_create.h>
+#include <chessboard_print_plain.h>
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, char** argv)
 {
+    Moves moves;
+    moves.count = 0;
     Chessboard chessboard;
     char string[(CHESSBOARD_SIZE + 1) * (CHESSBOARD_SIZE + 1) * 2 + 1];
+    char inputString[64];
+    char errstr[64];
+
+    if (argc == 1) {
+        fgets(inputString, 64, stdin);
+        if (parseStep(inputString, &moves, errstr)) {
+            printf("%s", errstr);
+            return 0;
+        }
+    }
     createChessboard(
             &chessboard,
             "rnbqkbnr"
@@ -16,6 +31,9 @@ int main(int argc, char** argv)
             "        "
             "PPPPPPPP"
             "RNBQKBNR");
+    for (int i = 0; i < moves.count; i++) {
+        doMove(moves.array[i], &chessboard);
+    }
     chessboardToString(&chessboard, string);
     printf("%s", string);
     return 0;
