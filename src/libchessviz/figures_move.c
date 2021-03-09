0cee3bb8d60edc2c7641af86287972e8868206b3
ApxuTechTop <78413120+ApxuTechTop@users.noreply.github.com>
2021-03-09 18:31:11 +0600

    Improve move function

--- src/libchessviz/figures_move.c
+++ src/libchessviz/figures_move.c
@@ -1,11 +1,54 @@
 #include "figures_move.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int doMove(const Move move, Chessboard* chessboard)
 {
-    chessboard->cells[move.to.letter - 'a'][move.to.number - '1']
-            = chessboard->cells[move.from.letter - 'a'][move.from.number - '1'];
-    chessboard->cells[move.to.letter - 'a'][move.to.number - '1'].side
-            = FigureSideNone;
-    chessboard->cells[move.to.letter - 'a'][move.to.number - '1'].type
-            = FigureTypeNone;
+    FieldNumber fn = move.from.number;
+    FieldLetter fl = move.from.letter;
+    FieldNumber tn = move.to.number;
+    FieldLetter tl = move.to.letter;
+    FigureSide fromSide = chessboard->cells[fn][fl].side;
+    FigureType fromType = chessboard->cells[fn][fl].type;
+    FigureSide toSide = chessboard->cells[tn][tl].side;
+    FigureType toType = chessboard->cells[tn][tl].type;
+    if (fromType == FigureTypeNone) {
+        return 1;
+    }
+    if (fromType != move.who) {
+        printf("(%d %d, %d %d)", fromType, move.who, fn, fl);
+        return 2;
+    }
+    if (move.type == MoveTypeCapture) {
+        if (fromSide == toSide) {
+            return 3;
+        }
+    } else {
+        if (toType != FigureTypeNone) {
+            return 4;
+        }
+    }
+    if (fromType == FigureTypePawn) {
+        if (move.type == MoveTypeQuiet && fl != tl) {
+            return 5;
+        }
+        switch ((tn - fn) * (fromSide == FigureSideWhite ? 1 : -1)) {
+        case 1:
+            break;
+        case 2:
+            if (fn != FieldNumber2 && fn != FieldNumber7) {
+                return 6;
+            }
+            break;
+        default:
+            return 7;
+        }
+    }
+
+    chessboard->cells[fn][fl].side = FigureSideNone;
+    chessboard->cells[fn][fl].type = FigureTypeNone;
+    chessboard->cells[tn][tl].side = fromSide;
+    chessboard->cells[tn][tl].type = fromType;
+
+    return 0;
 }
